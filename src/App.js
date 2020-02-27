d6f04404d5431ee63b4f275acaf5134d02976e73
Cristina Edens <edens.cristina@gmail.com>
2020-02-27 14:21:59 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1> 🦄  Web 27 Demo 🎆   </h1>
       <h2>
         Deployed with{' '}
         <a
